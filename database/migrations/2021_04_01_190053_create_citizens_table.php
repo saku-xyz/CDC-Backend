fbde7d6ed4ce0e860b685d7f0f13595b8d9288b0
SaKu <sakuofficial@gmail.com>
2021-04-02 00:47:00 +0530

    Initial Commit

---
+++ database/migrations/2021_04_01_190053_create_citizens_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitizensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citizens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('dob');
+            $table->string('telephone');
+            $table->string('email');
+            $table->string('address');
+            $table->string('lng');
+            $table->string('lat');
+            $table->string('health_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citizens');
+    }
+}
